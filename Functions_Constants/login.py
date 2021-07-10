ad12cd00589747b974162a9ea35c0b8677c70e1f
InconsistentBoi <kmodawel@gmail.com>
2021-07-10 20:55:50 +0530

    initial commit

---
+++ Functions_Constants/login.py
@@ -0,0 +1,36 @@
+import pygame,sys,os
+from pygame.locals import *
+from Functions_Constants import constants , Transition_moving, mfunc
+
+def input(event, input_num):
+    constants.charcount
+    if event.type == pygame.MOUSEBUTTONDOWN:
+        if constants.input_rect[input_num].collidepoint(event.pos):
+            constants.active[input_num] = True
+        else:
+            constants.active[input_num] = False
+
+            
+    if event.type==pygame.KEYDOWN:  
+        if constants.active[input_num] == True:
+            if event.key==pygame.K_BACKSPACE:
+                constants.charcount[input_num]-=1
+                constants.user_text[input_num] = constants.user_text[input_num][:-1]
+                constants.active[input_num] = True
+            else:
+                if constants.charcount[input_num] <= 10:
+                    constants.user_text[input_num] += event.unicode 
+                    constants.charcount[input_num]+=1
+
+
+
+def output(output_num):
+    if (output_num % 2):
+        render_text = '*' * len(constants.user_text[output_num])
+    else:
+        render_text = constants.user_text[output_num]
+    
+    text_surface = constants.base_font[output_num].render(render_text,True,(255,242,0))
+    constants.WIN.blit(text_surface,(constants.input_rect[output_num].x + 5, constants.input_rect[output_num].y + 5))
+    pygame.draw.rect(constants.WIN,constants.colour[output_num],constants.input_rect[output_num],4) 
+
